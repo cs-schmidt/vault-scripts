9d0f9a5b7537efcf243039613036d2a8a6ea20cd
cs-schmidt <ebs.euler@gmail.com>
2024-09-27 06:19:45 -0600

    refactor: simplify thought initialization function

--- src/templates/initializeThought.js
+++ src/templates/initializeThought.js
@@ -1,37 +1,19 @@
-/** The Templater user script used by the thought template to instantiate a thought. */
+import { requestOpenTitle, swapSlashes, isUniqueFilename } from '../utils/templates.js';
 
 // NOTE: Files created in Obsidian are always named "Untitled" initially.
-// NOTE: When "Show inline title" and "Show tab title bar" are toggled off you'll get an
-//       extra naming prompt when creating an entry.
-
-import { OPEN_TITLE_REGEX } from '../utils/constants.js';
-import isUniqueFilename from '../utils/isUniqueFilename.js';
+// NOTE: When "Show inline title" and "Show tab title bar" are turned off you'll get an
+//       extra naming prompt upon entry creation.
 
 /**
- * The initialization function used to instantiate thoughts. This procedure is used by
- * the thought template.
+ * The initialization function for the thought template.
  * @param {Object} tp - The Templater object.
- * @returns {Object} Data gathered from the user for instantiation.
- * @throws {Error} When any of the initialization procedures throw an error.
+ * @returns {Object} User-collected data for instantiation.
+ * @throws {Error} If any of the initialization steps throw an error.
  */
 export default async function initializeThought(tp) {
-  const result = { title: await requestTitle(tp) };
-  const filename = result.title.replaceAll(/[\/\\]/g, '-');
-  if (isUniqueFilename(filename)) await tp.file.rename(filename);
-  else throw Error("Creation Error: Title isn't unique.");
-  return result;
-}
-
-/**
- * Requests a title for the thought from the user.
- * @param {Object} tp - The Templater object.
- * @returns {string}
- * @throws {Error} When the input's format is improper or equal to Obsidian's default.
- */
-async function requestTitle(tp) {
-  const result = (await tp.system.prompt('Unique Title:', '', true)).trim();
-  if (!OPEN_TITLE_REGEX.test(result))
-    throw Error('Creation Error: Invalid title format.');
-  if (result === 'Untitled') throw Error('Creation Error: Default title is reserved.');
-  return result;
+  const title = await requestOpenTitle(tp);
+  const filename = swapSlashes(title, '-');
+  if (!isUniqueFilename(filename)) throw Error("Creation Error: Title isn't unique.");
+  await tp.file.rename(filename);
+  return { title };
 }
