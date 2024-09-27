0f1f41d4cf386e874d9473a8b9a4da2b863926ed
cs-schmidt <ebs.euler@gmail.com>
2024-09-27 04:26:31 -0600

    feat: update entry initialization function

--- src/templates/initializeEntry.js
+++ src/templates/initializeEntry.js
@@ -1,30 +1,31 @@
 import { Notice } from 'obsidian';
 
 // NOTE: Files created in Obsidian are always named "Untitled" initially.
-// NOTE: When "Show inline title" and "Show tab title bar" are toggled off you'll get an
-//       extra naming prompt when creating an entry.
+// NOTE: When "Show inline title" and "Show tab title bar" are turned off you'll get an
+//       extra naming prompt upon entry creation.
 
 /**
- * The initialization function used by the 'ENTRY' template to handle the beginning of
- * the entry creation process.
+ * The initialization function used by the 'ENTRY' template to produce an entry.
  * @param {Object} tp - The Templater object.
- * @returns {string}
- * @throws {Error} - When the user escapes selecting a template type or an error occurs
- *   during the on creation flow of a selected template.
+ * @returns {string} The entry's contents.
+ * @throws {Error} The user escaped template selection or theselected template's
+ *   initialization threw an error.
  */
 export default async function initializeEntry(tp) {
   try {
-    const choices = ['Inquiry', 'Informal', 'Formal', 'Thought', 'Practice', 'Log'];
-    const templates = choices.map((type) => `[[{T} ${type.toUpperCase()}]]`);
-    const template = await tp.system.suggester(choices, templates, true, 'Entry Type:');
+    /* Prompt the user to create an entry. */
+    const options = ['Inquiry', 'Informal', 'Formal', 'Thought', 'Practice', 'Log'];
+    const templates = options.map((option) => `[[{T} ${option.toUpperCase()}]]`);
+    const template = await tp.system.suggester(options, templates, true, 'Entry:');
     return await tp.file.include(template);
   } catch (err) {
+    /* Display the error and delete the target file.  */
     new Notice(err?.message || 'Something went wrong.');
-    // NOTE: It appears that deleting the target file synchronously causes templater to
-    //       get stuck in a "deletion-creation loop". So `setTimeout()` is being used as
-    //       a work around: I'm assuming that templater will be done processing the file
-    //       after a short delay and the file deletion will be safe.
+    // NOTE: You can't delete the target file before this function finishes executing,
+    //       otherwise templater gets stuck in a "deletion-creation loop". As a "work
+    //       around" I'm using `setTimeout()` with a closure around the target file
+    //       reference to execute deletion after a small delay.
     const targetFile = tp.config.target_file;
-    setTimeout(async () => await app.vault.delete(targetFile), 150);
+    setTimeout(() => app.vault.delete(targetFile), 100);
   }
 }
