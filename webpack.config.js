fe913a771bddf204327b014132249b8bd33d56f1
cs-schmidt <ebs.euler@gmail.com>
2024-10-05 12:36:03 -0600

    build: allow extensions to be removed from ESM imports

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,28 @@ import { fileURLToPath } from 'url';
 import CopyPlugin from 'copy-webpack-plugin';
 
 const projectRoot = dirname(fileURLToPath(import.meta.url));
-const buildDir = process.env['BUILD_DIR'];
+const buildPath = process.env['BUILD_PATH'];
 
 /** Webpack Configuration */
 export default {
   mode: 'production',
   context: projectRoot,
   entry: './src/main.js',
-  devtool: 'source-map',
   output: {
-    path: buildDir,
+    path: buildPath,
     filename: '[name].js',
     libraryTarget: 'commonjs',
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
   },
   externals: ['obsidian'],
   plugins: [new CopyPlugin({ patterns: [{ from: 'manifest.json' }] })],
