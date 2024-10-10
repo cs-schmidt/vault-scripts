63d902c32751eda5668dcf1cb65e88693beef6a3
cs-schmidt <ebs.euler@gmail.com>
2024-10-10 03:48:11 -0600

    feat: update internal Dataview functions

--- src/dataviews/internal.js
+++ src/dataviews/internal.js
@@ -1,59 +1,60 @@
 import { pageSchemas } from '../schemas';
-import { getAPI } from 'obsidian-dataview';
-
-const isLink = getAPI().value.isLink;
 
 // NOTE: To access the full Dataview API you must pass the Dataview object as an argument.
 //       Using `app.plugins.plugins.dataview.api` provides the DataviewAPI object instead
 //       of the DataviewInlineAPI object.
 
 /**
- * Show all inquiries with the same source key as the current entry.
- * @param {DataviewInlineAPI} dv - Dataview's inline API.
+ * Show all sibling inquiries.
+ * @param {DataviewInlineAPI} dv Dataview's inline API.
  * @returns {void}
  */
-export function showInquiryContentsTable(dv) {
-  const entry = dv.current();
-  const { error } = pageSchemas.inquiryPageSchema.validate(entry);
-  if (error) dv.paragraph(`*Invalid inquiry page: ${error.message}*`);
+export function showSiblingInquiries(dv) {
+  const note = dv.current();
+  const { error } = pageSchemas.inquiryPageSchema.validate(note);
+  if (error) dv.paragraph(`*Invalid inquiry: ${error.message}*`);
   else {
-    const type = entry.tags[0];
-    const sourceKey = entry['source-key'];
     const records = dv
-      .pages(type)
-      .filter((page) => page['source-key'] === sourceKey)
+      .pages('#inquiry')
+      .where(
+        (page) =>
+          !pageSchemas.inquiryPageSchema.validate(page).error &&
+          page['source-key'] === note['source-key'],
+      )
       .sort((page) => page.file.name, 'asc')
       .map((page) => [
         dv.paragraph(dv.fileLink(page.file.name)),
-        dv.paragraph(page.parents?.length ? page.parents : null),
+        dv.paragraph(page.parents.length ? page.parents : null),
         dv.paragraph(page['source-link'] || null),
         dv.paragraph(page.done ? '`true`' : '`false`'),
       ]);
     if (records.length)
       dv.table(['**Inquiry**', '**Parents**', '**Source Link**', '**Done**'], records);
-    else dv.span(`**No inquiries found under \`${sourceKey}\`**`);
+    else dv.paragraph(`*No inquiries found under \`${note['source-key']}\`*`);
   }
 }
 
 /**
- * Show all practice with the same source key as the current entry.
- * @param {DataviewInlineAPI} dv - Dataview's inline API.
+ * Show all sibling practice.
+ * @param {DataviewInlineAPI} dv Dataview's inline API.
  * @returns {void}
  */
-export function showPracticeContentsTable(dv) {
-  const entry = dv.current();
-  const { error } = pageSchemas.practicePageSchema.validate(entry);
-  if (error) dv.paragraph(`*Invalid practice page: ${error.message}*`);
+export function showSiblingPractice(dv) {
+  const note = dv.current();
+  const { error } = pageSchemas.practicePageSchema.validate(note);
+  if (error) dv.paragraph(`*Invalid practice: ${error.message}*`);
   else {
-    const type = entry.tags[0];
-    const sourceKey = entry['source-key'];
     const records = dv
-      .pages(type)
-      .filter((page) => page['source-key'] === sourceKey)
+      .pages('#practice')
+      .where(
+        (page) =>
+          !pageSchemas.practicePageSchema.validate(page).error &&
+          page['source-key'] === note['source-key'],
+      )
       .sort((page) => page.file.name, 'asc')
       .map((page) => [
         dv.paragraph(dv.fileLink(page.file.name)),
-        dv.paragraph(page.parents?.length ? page.parents : null),
+        dv.paragraph(page.parents.length ? page.parents : null),
         dv.paragraph(page['source-link'] || null),
         dv.paragraph(page['one-or-many'] ? `\`${page['one-or-many']}\`` : null),
         dv.paragraph(page.done ? '`true`' : '`false`'),
@@ -63,35 +64,61 @@ export function showPracticeContentsTable(dv) {
         ['**Practice**', '**Parents**', '**Source Link**', '**One-or-many**', '**Done**'],
         records,
       );
-    else dv.span(`**No inquiries found under \`${sourceKey}\`**`);
+    else dv.paragraph(`**No inquiries found under \`${note['source-key']}\`**`);
   }
 }
 
 /**
- * Display descendant informal entries.
- * @param {object} dv - The Dataview object.
+ * Shows all sibling formals.
+ * @param {object} dv The Dataview object.
+ * @returns {void}
+ */
+export function showSiblingFormals(dv) {
+  const note = dv.current();
+  const { error } = pageSchemas.formalPageSchema.validate(note);
+  if (error) dv.paragraph(`*Invalid formal: ${error.message}*`);
+  else {
+    const records = dv
+      .pages('#formal')
+      .filter((page) => {
+        if (pageSchemas.formalPageSchema.validate(page).error) return false;
+        const pageParentPaths = new Set(page.parents.map((link) => link.path));
+        return (
+          note.parents.length == pageParentPaths.size &&
+          note.parents.every((link) => pageParentPaths.has(link.path))
+        );
+      })
+      .map((page) => [
+        dv.paragraph(dv.fileLink(page.file.name, false, page.aliases?.[0])),
+        dv.paragraph(page.type ? `\`${page.type}\`` : null),
+        dv.paragraph(page.tags[1] || null),
+      ]);
+    if (records.length) dv.table(['**Formal**', '**Type**', '**Status**'], records);
+    else dv.paragraph('*No sibling formals found.*');
+  }
+}
+
+/**
+ * Shows descendant informals.
+ * @param {object} dv The Dataview object.
  * @returns {void}
  */
 export function showDescendantInformals(dv) {
-  const entry = dv.current();
-  const type = entry.tags?.[0];
-  const { error } = pageSchemas.informalPageSchema.validate(entry);
+  const note = dv.current();
+  const type = note.tags?.[0];
+  const { error } = pageSchemas.informalPageSchema.validate(note);
   if (type !== 'ℹ️' && error)
     dv.paragraph(
-      `Invalid page: ${
+      `*Invalid page: ${
         error.details.type === 'any.only' && type !== 'ℹ️'
           ? `\`tags\` should contain ℹ️, or ${error.message}`
           : `${error.message}`
-      }`,
+      }*`,
     );
   else {
     const records = dv
       .pages('#informal')
-      .filter(
-        (page) =>
-          page.parents instanceof Array &&
-          page.parents.some((value) => isLink(value) && value.path === entry.file.path),
-      )
+      .where((page) => !pageSchemas.informalPageSchema.validate(page).error)
       .map((page) => [
         dv.paragraph(dv.fileLink(page.file.name, false, page.aliases?.[0])),
         dv.paragraph(page.parents),
@@ -104,99 +131,114 @@ export function showDescendantInformals(dv) {
   }
 }
 
-// TODO: Implement `showFormalDescendant` function.
 /**
- * Display descendant formal entries.
- * @param {object} dv - The Dataview object.
+ * Shows all descendant formals.
+ * @param {object} dv The Dataview object.
  * @returns {void}
  */
-export function showSiblingFormals(dv) {
-  dv.span('*No descendant formal entries found.*');
-}
-
-/**
- * Display descendant thoughts entries.
- * @param {object} dv - The Dataview object.
- * @returns {void}
- */
-export function showDescendantThoughts(dv) {
-  const entry = dv.current();
-  const { error } = pageSchemas.informalPageSchema.validate(entry);
-  if (error) dv.paragraph(`*Invalid informal page: ${error.message}*`);
+export function showDescendantFormals(dv) {
+  const note = dv.current();
+  const { error } = pageSchemas.informalPageSchema.validate(note);
+  if (error) dv.paragraph(`*Invalid informal: ${error.message}*`);
   else {
     const records = dv
-      .pages('#thought')
-      .filter(
+      .pages('#formal')
+      .where(
         (page) =>
-          page.parents instanceof Array &&
-          page.parents.some((value) => isLink(value) && value.path === entry.file.path),
+          !pageSchemas.formalPageSchema.validate(page).error &&
+          page.parents.some((link) => link.path === note.file.path),
       )
       .map((page) => [
         dv.paragraph(dv.fileLink(page.file.name, false, page.aliases?.[0])),
         dv.paragraph(page.parents),
-        dv.paragraph(page.tags?.[1] || null),
+        dv.paragraph(page.type ? `\`${page.type}\`` : null),
+        dv.paragraph(page.tags[1] || null),
       ]);
-    if (records.length) dv.table(['**Thought**', '**Parents**', '**Status**'], records);
-    else dv.span('*No descendant thoughts found.*');
+    if (records.length)
+      dv.table(['**Formal**', '**Parents**', '**Type**', '**Status**'], records);
+    else dv.paragraph('*No descendant formals found.*');
   }
 }
 
 /**
- * Display "top-level" list items under the "Comments and Questions" section of other
- * entries which also contain a link to the current entry.
- * @param {DataviewInlineAPI} dv - Dataview's inline API.
+ * Shows all descendant thoughts.
+ * @param {object} dv The Dataview object.
  * @returns {void}
  */
-export function showOtherCommentsAndQuestions(dv) {
-  // NOTE: Unguardable edge case: if a "Comments and Questions" header isn't a top level
-  //       heading or occurs more than once, then list items under it will show up.
-  const currentFile = dv.current().file;
-  const targetSection = 'Comments and Questions';
-  const records = dv
-    .pages(`[[${currentFile.name}]] AND !"${currentFile.path}"`)
-    .flatMap(({ file: { aliases, name, lists } }) =>
-      lists
-        .filter(
-          (listItem) =>
-            !listItem.task &&
-            listItem.section.subpath?.trim() === targetSection &&
-            listItem.outlinks.some((link) => link.path === currentFile.name),
-        )
-        .map((listItem) => [
-          listItem.text,
-          dv.sectionLink(name, targetSection, false, aliases?.[0] || name),
-        ]),
-    );
-  if (records.length) dv.table(['**Comment/Question**', '**Origin**'], records);
-  else dv.span('*No external comments or questions found.*');
+export function showDescendantThoughts(dv) {
+  const note = dv.current();
+  const { error } = pageSchemas.informalPageSchema.validate(note);
+  if (error) dv.paragraph(`*Invalid informal: ${error.message}*`);
+  else {
+    const records = dv
+      .pages('#thought')
+      .where(
+        (page) =>
+          !pageSchemas.thoughtPageSchema.validate(page).error &&
+          page.parents.some((link) => link.path === note.file.path),
+      )
+      .map((page) => [
+        dv.paragraph(dv.fileLink(page.file.name, false, page.aliases?.[0])),
+        dv.paragraph(page.parents),
+        dv.paragraph(page.tags[1] || null),
+      ]);
+    if (records.length) dv.table(['**Thought**', '**Parents**', '**Status**'], records);
+    else dv.paragraph('*No descendant thoughts found.*');
+  }
 }
 
 /**
- * Display "top-level" tasks under the "Tasks" section of other entries which also
- * contain a link to the current entry.
- * @param {DataviewInlineAPI} dv - Dataview's inline API.
+ * Shows "top-level" list items under the "Comments and Questions" section of other
+ * entries which also contain a link to the current entry.
+ * @param {DataviewInlineAPI} dv Dataview's inline API.
  * @returns {void}
  */
-export function showOtherTasks(dv) {
+export function showOuterCommentsAndQuestions(dv) {
+  // NOTE: Unguardable edge case: if a "Comments and Questions" header isn't a top level
+  //       heading or occurs more than once, then list items under it will show up.
+  const file = dv.current().file;
+  const section = 'Comments and Questions';
+  const records = dv.pages(`[[${file.name}]] AND !"${file.path}"`).flatMap((page) => {
+    const name = page.file.name;
+    const alias = page.file.aliases?.[0];
+    const list = page.file.lists;
+    list
+      .filter(
+        (listItem) =>
+          !listItem.task &&
+          listItem.section.subpath.trim() == section &&
+          listItem.outlinks.some((link) => link.path == name),
+      )
+      .map(({ text }) => [text, dv.sectionLink(name, section, false, alias || name)]);
+  });
+  if (records.length) dv.table(['**Comment/Question**', '**Origin**'], records);
+  else dv.paragraph('*No outer comments or questions found.*');
+}
+
+/**
+ * Shows "top-level" tasks under the "Tasks" section of other entries which also contain a
+ * link to the current entry.
+ * @param {DataviewInlineAPI} dv Dataview's inline API.
+ * @returns {void}
+ */
+export function showOuterTasks(dv) {
   // NOTE: Unguardable edge case: if a "Tasks" header isn't a top level heading or occurs
   //       more than once, then list items under it will show up.
-  const currentFile = dv.current().file;
-  const targetSection = 'Tasks';
-  const records = dv
-    .pages(`[[${currentFile.name}]] AND !"${currentFile.path}"`)
-    .flatMap(({ file: { aliases, name, lists } }) =>
-      lists
-        .filter(
-          (listItem) =>
-            listItem.task &&
-            listItem.section.subpath?.trim() === targetSection &&
-            listItem.outlinks.some((link) => link.path === currentFile.name),
-        )
-        .map((listItem) => [
-          listItem.text,
-          dv.sectionLink(name, targetSection, false, aliases?.[0] || name),
-        ]),
-    );
+  const file = dv.current().file;
+  const section = 'Tasks';
+  const records = dv.pages(`[[${file.name}]] AND !"${file.path}"`).flatMap((page) => {
+    const name = page.file.name;
+    const alias = page.file.aliases?.[0];
+    const list = page.file.lists;
+    list
+      .filter(
+        (listItem) =>
+          listItem.task &&
+          listItem.section.subpath.trim() == section &&
+          listItem.outlinks.some((link) => link.path == name),
+      )
+      .map(({ text }) => [text, dv.sectionLink(name, section, false, alias || name)]);
+  });
   if (records.length) dv.table(['**Task**', '**Origin**'], records);
-  else dv.span('*No external tasks found.*');
+  else dv.paragraph('*No outer tasks found.*');
 }
